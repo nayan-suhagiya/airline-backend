87fc0d703799380787aa6e08781a8e328c7545a8
nayan-suhagiya <suhagiya.nayan01@gmail.com>
2024-01-15 23:30:35 +0530

    add valid user api

--- router/auth.routes.js
+++ router/auth.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authController from "../controller/auth.controller.js";
+import Auth from "../middleware/auth.middleware.js";
 const authRouter = Router();
 
 authRouter.post("/register", authController.registerUser);
 authRouter.post("/login", authController.loginUser);
+authRouter.get("/validuser", Auth, authController.getValidUser);
 
 export default authRouter;
